df076bb8757b95aed2ca597afd917f70f7631a78
Maksim-07 <kosmakovmaksim@yandex.ru>
2024-12-04 14:54:42 +0300

    Deleted unnecessary code

--- src/api/user.py
+++ src/api/user.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 
 from schemas.users import UserIdSchema
-from services.message import MessageService
 from services.user import UserService
 
 router = APIRouter(prefix="/users", tags=["User"])
@@ -10,8 +9,3 @@ router = APIRouter(prefix="/users", tags=["User"])
 @router.get("/{login}", response_model=UserIdSchema)
 async def get_id(login: str, user_service: UserService = Depends()) -> UserIdSchema:
     return await user_service.get_user_id(login=login)
-
-
-# @router.get("/all-message")
-# async def get_mess(message: MessageService = Depends()):
-#     return await message.get_all_messages()

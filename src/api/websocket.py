f083da7f16d2cff5f170c803a3c897e0ad2e572c
Maksim-07 <kosmakovmaksim@yandex.ru>
2024-12-05 14:30:33 +0300

    Fixed the bugs

--- src/api/websocket.py
+++ src/api/websocket.py
@@ -1,18 +1,10 @@
-from functools import lru_cache
-
 from fastapi import APIRouter, Depends, WebSocket
 
-from services.connection_websocket import ConnectionManagerService, connection_manager
 from services.message import MessageService
 
 router = APIRouter(prefix="/ws", tags=["WebSocket"])
 
 
 @router.websocket("/{client_id}")
-async def websocket_endpoint(
-    websocket: WebSocket,
-    client_id: int,
-    message_service: MessageService = Depends(),
-    connection: ConnectionManagerService = Depends(lru_cache(connection_manager)),
-) -> None:
-    await message_service.process_websocket(websocket, client_id, connection)
+async def websocket_endpoint(websocket: WebSocket, client_id: int, message_service: MessageService = Depends()) -> None:
+    await message_service.process_websocket(websocket=websocket, client_id=client_id)
--- src/services/message.py
+++ src/services/message.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 from typing import Sequence
 
 from fastapi import Depends, WebSocket, WebSocketDisconnect
@@ -17,10 +16,12 @@ class MessageService:
         message_repo: MessageRepository = Depends(),
         user_repo: UserRepository = Depends(),
         user_service: UserService = Depends(),
+        connection: ConnectionManagerService = Depends(connection_manager),
     ):
         self.message_repo = message_repo
         self.user_repo = user_repo
         self.user_service = user_service
+        self.connection = connection
 
     async def add_message(self, message: MessageSchema) -> None:
         await self.message_repo.add(sender_id=message.sender_id, message=message.message)
@@ -33,13 +34,8 @@ class MessageService:
 
         return await self.message_repo.get_messages_by_sender_id(user_id)
 
-    async def process_websocket(
-        self,
-        websocket: WebSocket,
-        client_id: int,
-        connection: ConnectionManagerService = Depends(lru_cache(connection_manager)),
-    ) -> None:
-        await connection.connect(websocket)
+    async def process_websocket(self, websocket: WebSocket, client_id: int) -> None:
+        await self.connection.connect(websocket)
 
         user_name = await self.user_repo.get_login_by_id(user_id=client_id)
 
@@ -49,12 +45,11 @@ class MessageService:
         all_messages = await self.message_repo.get_all_messages()
         for m in all_messages:
             message = CurrentMessageSchema(login=m[0], message=m[1])
-            await connection.send_personal_message(message, websocket)
+            await self.connection.send_personal_message(message, websocket)
 
-        connect_message = CurrentMessageSchema(message=f"{user_name} присоединился(-лась) к чату")
-        disconnect_message = CurrentMessageSchema(message=f"{user_name} вышел(-ла) из чата")
-
-        await connection.broadcast(connect_message, websocket)
+        await self.connection.broadcast(
+            CurrentMessageSchema(message=f"{user_name} присоединился(-лась) к чату"), websocket
+        )
 
         try:
             while True:
@@ -62,13 +57,13 @@ class MessageService:
 
                 message = CurrentMessageSchema(login=user_name, message=data)
 
-                await connection.send_personal_message(message, websocket)
+                await self.connection.send_personal_message(message, websocket)
 
-                await connection.broadcast(message, websocket)
+                await self.connection.broadcast(message, websocket)
 
                 await self.add_message(MessageSchema(sender_id=client_id, message=data))
 
         except WebSocketDisconnect:
-            connection.disconnect(websocket)
+            self.connection.disconnect(websocket)
 
-            await connection.broadcast(disconnect_message, websocket)
+            await self.connection.broadcast(CurrentMessageSchema(message=f"{user_name} вышел(-ла) из чата"), websocket)

3780288bb09d363de083877854feffc5a2e00d9f
Maksim-07 <kosmakovmaksim@yandex.ru>
2024-12-17 12:06:34 +0300

    Moved verify_token_from_query to core/auth

--- src/api/router.py
+++ src/api/router.py
@@ -3,12 +3,11 @@ from fastapi import APIRouter, Depends
 from api.auth import router as auth_router
 from api.user import router as user_router
 from api.websocket import router as websocket_router
-from core.auth import get_and_verify_token
-from services.connection_websocket import get_and_verify_token_from_query
+from core.auth import verify_token_from_query, verify_token_from_header
 
 router = APIRouter(prefix="/api")
 
 router.include_router(auth_router)
-router.include_router(websocket_router, dependencies=[Depends(get_and_verify_token_from_query)])
+router.include_router(websocket_router, dependencies=[Depends(verify_token_from_query)])
 router.include_router(websocket_router)
-router.include_router(user_router, dependencies=[Depends(get_and_verify_token)])
+router.include_router(user_router, dependencies=[Depends(verify_token_from_header)])
--- src/core/auth.py
+++ src/core/auth.py
@@ -1,17 +1,18 @@
 import jwt
-from fastapi import Depends, Request
+from fastapi import Depends, Query, Request
 from jwt import InvalidTokenError
 
 from core.config import settings
 from core.exceptions import (
     credentials_exception,
     invalid_token_exception,
+    token_not_found_exception,
     user_not_found_exception,
 )
 from db.repository.user import UserRepository
 
 
-async def get_and_verify_token(request: Request, user_repo: UserRepository = Depends()) -> str:
+async def verify_token_from_header(request: Request, user_repo: UserRepository = Depends()) -> None:
     token = request.headers.get("Authorization")
     try:
         payload = jwt.decode(token, settings().SECRET_KEY, algorithms=[settings().ALGORITHM])
@@ -27,4 +28,23 @@ async def get_and_verify_token(request: Request, user_repo: UserRepository = Dep
     except InvalidTokenError:
         raise invalid_token_exception
 
-    return token
+
+async def verify_token_from_query(
+    token: str = Query(None, alias="token"), user_repo: UserRepository = Depends()
+) -> None:
+    if not token:
+        raise token_not_found_exception
+
+    try:
+        payload = jwt.decode(token, settings().SECRET_KEY, algorithms=[settings().ALGORITHM])
+        user_id: int = payload.get("user_id")
+
+        if user_id is None:
+            raise credentials_exception
+
+        user = await user_repo.get_by(id=user_id)
+        if user is None:
+            raise user_not_found_exception
+
+    except InvalidTokenError:
+        raise invalid_token_exception
